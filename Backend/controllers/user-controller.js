371bb9d7d61e25abd0a49682069bcf71e6b4ef1e
Ankit <ankitsuneja456.com>
2023-06-28 16:58:35 +0530

    ignore

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -15,4 +15,46 @@ const getAllUser= async(req, res, next)=>{
     return res.status(200).json({users});
 }
 
+const signup = async(req, res, next)=>{
+    const {name, email, password} = req.body
+    
+    let existingUser;
+    try {
+        existingUser= await user.findOne({email})
+
+    }catch(err){
+    console.log(err);
+    }
+    if (existingUser){
+        return res.status(400).json({message: "user already exists! Login instead"})
+
+    }
+    const user= new User({
+        name,
+        email,
+        password
+    });
+
+    try{
+        await user.save();
+
+    }catch(err){
+        console.log(err);
+    }
+    return res.status(201).json({user})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports= signup
 module.exports= getAllUser
--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -1,9 +1,11 @@
 const express = require ("express");
 
 const  getAllUser  = require("../controllers/user-controller");
+const signup = require("../controllers/user-controller")
 
 const router= express.Router();
 
 router.get("/", getAllUser)
+router.post("/signup", signup)
 
 module.exports= router;
