90f2e5c4817b528b5b9e6dbae28c288adddf767b
Ankit <ankitsuneja456.com>
2023-06-30 01:03:34 +0530

    blog

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const User = require("../model/user")
 const bcrypt = require("bcryptjs")
 
+
  const getAllUser= async(req, res, next)=>{
     let users;
     try{
@@ -44,7 +45,28 @@ const signup = async(req, res, next)=>{
     return res.status(201).json({newUser})
 }
 
+const login = async(req, res, next)=>{
+    const {email, password} = req.body
+    let existingUser;
+    try {
+        existingUser= await User.findOne({email})
+    }catch(err){
+    return console.log(err);
+    }
+    if (!existingUser){
+        return res.status(404).json({message: "couldn't find user by this email"})
+
+    }
+
+    const isPasswordCorrect= bcrypt.compareSync(password, existingUser.password)
+    if(!isPasswordCorrect){
+        return res.status(400).json({message: "Incorrect password"})
+    }
+    return res.status(200).json({message: "Login Succesful!"})
+
+}
 
 
 
-module.exports = { signup, getAllUser };
+
+module.exports = { signup, getAllUser, login };
--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -1,9 +1,10 @@
 const express = require ("express");
-const { getAllUser, signup } = require("../controllers/user-controller");
+const { getAllUser, signup, login } = require("../controllers/user-controller");
 
 const userRoutes= express.Router();
 
-userRoutes.get("/", getAllUser)
-userRoutes.post("/signup", signup)
+userRoutes.get("/", getAllUser);
+userRoutes.post("/signup", signup);
+userRoutes.post("/login", login)
 
 module.exports= userRoutes;
