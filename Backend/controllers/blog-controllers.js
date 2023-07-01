0f8b788fca545f6a642356c2ee959875ab947c1e
Ankit <ankitsuneja456.com>
2023-07-01 18:47:26 +0530

    getByIdNotWorking

--- Backend/controllers/blog-controllers.js
+++ Backend/controllers/blog-controllers.js
@@ -31,7 +31,37 @@ const addBlog = async(req, res, next)=>{
         }
         return res.status(200).json({blog})
 }
+const updateBlog = async(req, res, next)=>{
+    const {title, description}= req.body
+    const BlogId= req.params.id 
+    let blog;
+    try{
+        blog = await Blog.findByIdAndUpdate(BlogId, {
+            title,
+            description
+        })
+    }catch(err){
+    return console.log(err);
+}
+if(!blog){
+    return res.status(500).json({message:"unable to update blog"})
+}return res.status(200).json({blog})
+}
+
+const getById = async(req, res, next)=>{
+    const {id}= req.params.id
+    let blog;
+    try{ 
+        blog = await Blog.findById(id);
+
+    }catch(err){
+        return console.log(err);
+    }
+    if(!blog){
+        return res.status(400).json({message: "blog not found"})
+    }return res.status(200).json({blog})
+}
 
 
 
-module.exports = { getAllBlog, addBlog }
+module.exports = { getAllBlog, addBlog, updateBlog, getById }
--- Backend/routes/blog-routes.js
+++ Backend/routes/blog-routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAllBlog, addBlog } = require("../controllers/blog-controllers")
+const { getAllBlog, addBlog, updateBlog, getById } = require("../controllers/blog-controllers")
 
 const blogRoutes = express.Router();
 
 blogRoutes.get("/", getAllBlog);
-blogRoutes.post("/add", addBlog)
+blogRoutes.post("/add", addBlog);
+blogRoutes.put("/update/:id", updateBlog)
+blogRoutes.get("/:id", getById)
 
 module.exports = blogRoutes; 
